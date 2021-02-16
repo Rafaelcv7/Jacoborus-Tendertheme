7a5696fbcfae0abc2b3f9b2837b58d80a5ceaae3
Rafael Cedeno Vazquez <55097377+Rafaelcv7@users.noreply.github.com>
2021-02-15 22:51:54 -0400

    Create tender.js

---
+++ tender.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#0x282828", "#0xf43753", "#0xc9d05c", "#0cxffc24b", "#0xb3deef", "#0xd3b987", "#0x73cef4", "#0xeeeeee", "#0x4c4c4c", "#0xf43753", "#0xc9d05c", "#0xffc24b", "#0xb3deef", "#0xd3b987", "#0x73cef4", "#0xfeffff"]);
+t.prefs_.set('foreground-color', "#0xeeeeee");
+t.prefs_.set('background-color', "#0x282828");
+t.prefs_.set('cursor-color', 'rgba(239,191,56,0.5)');
