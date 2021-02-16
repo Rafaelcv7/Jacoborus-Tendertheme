c75ea788f7f3f8e76c3ebb1ecdfd977b90828628
Rafael Cedeno Vazquez <55097377+Rafaelcv7@users.noreply.github.com>
2021-02-15 23:23:58 -0400

    Update tender.js

--- tender.js
+++ tender.js
@@ -1,4 +1,26 @@
-t.prefs_.set('color-palette-overrides',["#282828", "#f43753", "#c9d05c", "#ffc24b", "#b3deef", "#d3b987", "#73cef4", "#eeeeee", "#4c4c4c", "#f43753", "#c9d05c", "#ffc24b", "#b3deef", "#d3b987", "#73cef4", "#feffff"]);
+black       = '#282828';
+red         = '#f43753'; // red
+green       = '#c9d05c'; // green
+yellow      = '#ffc24b'; // yellow
+blue        = '#b3deef'; // blue
+magenta     = '#d3b987'; // pink
+cyan        = '#73cef4'; // cyan
+white       = '#eeeeee'; // light gray
+lightBlack  = '#4c4c4c'; // medium gray
+lightRed    = '#f43753'; // red
+lightGreen  = '#c9d05c'; // green
+lightYellow = '#ffc24b'; // yellow
+lightBlue   = '#b3deef'; // blue
+lightMagenta= '#d3b987'; // pink
+lightCyan   = '#73cef4'; // cyan
+lightWhite  = '#feffff'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
 t.prefs_.set('foreground-color', "#eeeeee");
 t.prefs_.set('background-color', "#282828");
 t.prefs_.set('cursor-color', 'rgba(239,191,56,0.5)');
